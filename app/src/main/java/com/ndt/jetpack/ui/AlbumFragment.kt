735290b9892c86603284c55e746248ad55f08ef0
ndt0409 <vivucenter@gmail.com>
2022-01-16 10:46:42 +0700

    Share data between view model with view model provider

--- app/src/main/java/com/ndt/jetpack/ui/AlbumFragment.kt
+++ app/src/main/java/com/ndt/jetpack/ui/AlbumFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.ndt.jetpack.databinding.FragmentAlbumBinding
+import com.ndt.jetpack.viewmodel.MusicViewModel
 
 class AlbumFragment : Fragment() {
+    private lateinit var viewModel: MusicViewModel
     private lateinit var binding: FragmentAlbumBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +18,14 @@ class AlbumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAlbumBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
+        //viewModel = ViewModelProvider(requireActivity()).get(MusicViewModel::class.java) //dung chung context cua music viewmodel
+        binding.btnUp.setOnClickListener {
+            viewModel.increaseNumber()
+        }
+        viewModel.number.observe(viewLifecycleOwner, {
+            binding.tvNumber.text = it.toString()
+        })
         return binding.root
     }
 }
--- app/src/main/java/com/ndt/jetpack/ui/SongFragment.kt
+++ app/src/main/java/com/ndt/jetpack/ui/SongFragment.kt
@@ -5,16 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.ndt.jetpack.databinding.FragmentSongBinding
+import com.ndt.jetpack.viewmodel.MusicViewModel
 
-class SongFragment:Fragment() {
+class SongFragment : Fragment() {
     private lateinit var binding: FragmentSongBinding
+    private lateinit var viewModel: MusicViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSongBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
+        //viewModel = ViewModelProvider(requireActivity()).get(MusicViewModel::class.java) //dung chung context cua music viewmodel
+        viewModel.number.observe(viewLifecycleOwner, {
+            binding.tvNumber.text = it.toString()
+        })
+        binding.btnUp.setOnClickListener {
+            viewModel.increaseNumber()
+        }
         return binding.root
     }
 }
