4cdf5e9ea5108782b6d4b8b26e1528a5f2f985f0
ndt0409 <vivucenter@gmail.com>
2022-01-16 20:34:30 +0700

    create view model (ktx)

--- app/src/main/java/com/ndt/jetpack/ui/AlbumFragment.kt
+++ app/src/main/java/com/ndt/jetpack/ui/AlbumFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.ndt.jetpack.databinding.FragmentAlbumBinding
 import com.ndt.jetpack.viewmodel.MusicViewModel
 
 class AlbumFragment : Fragment() {
-    private lateinit var viewModel: MusicViewModel
+    //private val viewModel: MusicViewModel by viewModels()        //no sharing data
+    private val viewModel: MusicViewModel by activityViewModels() //sharing data
+    //private lateinit var viewModel:MusicViewModel
     private lateinit var binding: FragmentAlbumBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,7 +22,7 @@ class AlbumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAlbumBinding.inflate(layoutInflater)
-        viewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
+        //viewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
         //viewModel = ViewModelProvider(requireActivity()).get(MusicViewModel::class.java) //dung chung context cua music viewmodel
         binding.btnUp.setOnClickListener {
             viewModel.increaseNumber()
--- app/src/main/java/com/ndt/jetpack/ui/SongFragment.kt
+++ app/src/main/java/com/ndt/jetpack/ui/SongFragment.kt
@@ -5,20 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.ndt.jetpack.databinding.FragmentSongBinding
 import com.ndt.jetpack.viewmodel.MusicViewModel
 
 class SongFragment : Fragment() {
     private lateinit var binding: FragmentSongBinding
-    private lateinit var viewModel: MusicViewModel
+
+    //private lateinit var viewModel: MusicViewModel
+    //private val viewModel by viewModels<MusicViewModel>()       // No sharing data
+    private val viewModel by activityViewModels<MusicViewModel>() //sharing data
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSongBinding.inflate(layoutInflater)
-        viewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
+        //viewModel = ViewModelProvider(this).get(MusicViewModel::class.java)
         //viewModel = ViewModelProvider(requireActivity()).get(MusicViewModel::class.java) //dung chung context cua music viewmodel
         viewModel.number.observe(viewLifecycleOwner, {
             binding.tvNumber.text = it.toString()
