2c667a72fdb921a487ce759be819a03d72c3d13a
ndt0409 <vivucenter@gmail.com>
2022-01-20 09:08:58 +0700

    handle onetime event in view  model

--- app/src/main/java/com/ndt/jetpack/ui/SongFragment.kt
+++ app/src/main/java/com/ndt/jetpack/ui/SongFragment.kt
@@ -1,15 +1,22 @@
 package com.ndt.jetpack.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.ndt.jetpack.MainActivity
 import com.ndt.jetpack.databinding.FragmentSongBinding
 import com.ndt.jetpack.viewmodel.MusicViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.observeOn
+import kotlinx.coroutines.launch
 
 class SongFragment : Fragment() {
     private lateinit var binding: FragmentSongBinding
@@ -29,8 +36,32 @@ class SongFragment : Fragment() {
             binding.tvNumber.text = it.toString()
         })
         binding.btnUp.setOnClickListener {
-            viewModel.increaseNumber()
+//            viewModel.increaseNumber()
+//            viewModel.showToast()
+            //viewModel.startActivity()
+            viewModel.showToastChannel()
         }
+        viewModel.needToast.observe(viewLifecycleOwner, {
+            if (it) {
+                //  Toast.makeText(requireContext(), "New toast", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        viewModel.nextActivity.observe(viewLifecycleOwner, {
+            it.getContentIfNotHandled()?.let {
+                val intent = Intent(requireActivity(), MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+        lifecycleScope.launch {
+            viewModel.showToastChannel.collect {
+                if (it) {
+                    Toast.makeText(requireContext(), "New Toast", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
         return binding.root
     }
 }
--- app/src/main/java/com/ndt/jetpack/viewmodel/MusicViewModel.kt
+++ app/src/main/java/com/ndt/jetpack/viewmodel/MusicViewModel.kt
@@ -1,17 +1,46 @@
 package com.ndt.jetpack.viewmodel
 
+import android.app.usage.UsageEvents
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ndt.jetpack.util.Event
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
-class MusicViewModel:ViewModel() {
-    private val _number= MutableLiveData<Int>()
-    val number:LiveData<Int> get() = _number
+class MusicViewModel : ViewModel() {
+    private val _number = MutableLiveData<Int>()
+    val number: LiveData<Int> get() = _number
+
+    private val _needToast = MutableLiveData<Boolean>()
+    val needToast: LiveData<Boolean> get() = _needToast
+
+    private val _nextActivity = MutableLiveData<Event<Boolean>>()
+    val nextActivity : LiveData<Event<Boolean>> get() = _nextActivity
+
+    private val _needToastChannel = Channel<Boolean>()
+    val showToastChannel = _needToastChannel.receiveAsFlow()
 
     init {
         _number.postValue(0)
     }
-    fun increaseNumber(){
+
+    fun showToast() {
+        _needToast.postValue(true)
+    }
+    fun showToastChannel(){
+        viewModelScope.launch {
+            _needToastChannel.send(true)
+        }
+    }
+
+    fun startActivity(){
+        _nextActivity.postValue(Event(true))
+    }
+
+    fun increaseNumber() {
         _number.postValue(number.value?.plus(1))
     }
 }
